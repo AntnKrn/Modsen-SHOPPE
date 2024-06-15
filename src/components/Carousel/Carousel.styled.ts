6d03cfee94a70893e704d244da0591ff154dbe7c
Anton <krnant@icloud.com>
2024-06-15 22:17:07 +0100

    update carousel

--- src/components/Carousel/Carousel.styled.ts
+++ src/components/Carousel/Carousel.styled.ts
@@ -5,16 +5,43 @@ export const ImgSlider = styled.img`
   width: 100%;
   height: 646px;
   display: block;
+  border-radius: 16px;
 `;
 
-export const ImgSliderButton = styled.button`
-  all: unset;
+export const ButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
   position: absolute;
-  top: 0;
+  gap: 13.71px;
   bottom: 0;
+  left: 0;
+  right: 0;
+  margin: 0 auto 26.29px;
+`;
+
+export const ImgSliderButton = styled.button<{ $number?: number }>`
   cursor: pointer;
-  padding: 3rem;
-  transition: background-color 100ms ease-in-out;
+  height: 9.14px;
+  width: 9.14px;
+  border-radius: 50%;
+  background-color: rgba(255, 255, 255, 1);
+  border: none;
+  transform: scale(1);
+  transition: all 0.7s ease;
+
+  &:hover {
+    border: 1px solid rgba(255, 255, 255, 1);
+    transform: scale(1.75);
+    transition: all 0.5s ease;
+    background-color: rgba(28, 28, 28, 0);
+  }
+  &.active {
+    border: 1px solid rgba(255, 255, 255, 1);
+    transform: scale(1.75);
+    transition: all 0.5s ease;
+    background-color: rgba(28, 28, 28, 0);
+  }
 `;
 
 export const ImgWrapper = styled.div`
--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -3,47 +3,54 @@ import React, { useEffect, useState } from 'react';
 import img1 from './img1.jpg';
 import img2 from './img2.jpg';
 import img3 from './img3.jpg';
-import { ImgSlider, ImgSliderButton, ImgWrapper } from './Carousel.styled';
+import img4 from './img4.jpg';
+import img5 from './img5.jpg';
+import {
+  ButtonWrapper,
+  ImgSlider,
+  ImgSliderButton,
+  ImgWrapper,
+} from './Carousel.styled';
 
 export const Carousel = () => {
-  const imagesUrls: string[] = [img1, img2, img3];
+  const imagesUrls: string[] = [img1, img2, img3, img4, img5];
 
   const [imageIndex, setImageIndex] = useState(0);
 
   useEffect(() => {
+    const imgSliderButton = document.getElementsByClassName(
+      `img${imageIndex + 1}`,
+    )[0];
+
     const toggle = setInterval(() => {
       setImageIndex((index) => {
+        if (imgSliderButton) {
+          imgSliderButton.classList.remove('active');
+        }
         if (index === imagesUrls.length - 1) return 0;
         return index + 1;
       });
     }, 4000);
 
+    if (imgSliderButton) {
+      imgSliderButton.classList.add('active');
+    }
     return () => clearInterval(toggle);
   });
-
-  const showNextImage = () => {
-    setImageIndex((index) => {
-      if (index === imagesUrls.length - 1) return 0;
-      return index + 1;
-    });
-  };
-
-  const showPrevImage = () => {
-    setImageIndex((index) => {
-      if (index === 0) return imagesUrls.length - 1;
-      return index - 1;
-    });
-  };
 
   return (
     <ImgWrapper>
       <ImgSlider src={imagesUrls[imageIndex]} alt="" />
-      <ImgSliderButton onClick={showPrevImage} style={{ left: 0 }}>
-        &lt;
-      </ImgSliderButton>
-      <ImgSliderButton onClick={showNextImage} style={{ right: 0 }}>
-        &gt;
-      </ImgSliderButton>
+      <ButtonWrapper>
+        {imagesUrls.map((img, index) => (
+          <ImgSliderButton
+            onClick={() => setImageIndex(index)}
+            key={img}
+            $number={index}
+            className={`img${index + 1} ${index === 0 ? 'active' : 0}`}
+          />
+        ))}
+      </ButtonWrapper>
     </ImgWrapper>
   );
 };

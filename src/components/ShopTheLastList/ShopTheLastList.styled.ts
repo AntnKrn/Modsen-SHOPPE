72b6d027bd917682625f30b56afff586ce6470cb
Anton <krnant@icloud.com>
2024-06-16 11:14:21 +0100

    fixed ShopTheLastList

--- src/components/ShopTheLastList/ShopTheLastList.styled.ts
+++ src/components/ShopTheLastList/ShopTheLastList.styled.ts
@@ -5,6 +5,7 @@ export const ShopTheLastListWrapper = styled.div`
   width: 100%;
   display: grid;
   grid-template-columns: repeat(3, auto);
+  grid-template-rows: repeat(2, auto);
   justify-content: space-between;
   row-gap: 86px;
 `;
